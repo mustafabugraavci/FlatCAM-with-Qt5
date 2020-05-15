19840eb6f06e8595be02c45f646270705438e578
Mustafa Buğra Avcı <mbugraavci38@gmail.com>
2020-05-15 19:50:27 +0300

    Add files via upload

---
+++ setup_ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+# "-e" exists on first error.
+
+sudo apt-get install libpng-dev \
+                libfreetype6 \
+				libfreetype6-dev \
+				python3-dev \
+				python3-simplejson \
+				python3-pyqt4 \
+				python3-numpy \
+				python3-scipy \
+				python3-matplotlib \
+				libgeos-dev \
+				python-shapely \
+				python-pip \
+				libspatialindex-dev \
+				python3-tk \
+				python3-shapely \
+				python3-rtree \
+				python3-svg.path
+
